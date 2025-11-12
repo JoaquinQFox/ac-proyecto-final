d1c72efa5cb91f47dd220a2f95133171fd12ef5e
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-11-12 00:01:26 -0500

    game: se corrige errores en las posiciones de bloques

--- src/tetris_game/blocks.py
+++ src/tetris_game/blocks.py
@@ -8,8 +8,9 @@ class LBlock(Block):
             0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
             1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
-            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 2)]
+            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
         }
+        self.move(0, 3)
 
 class JBlock(Block):
     def __init__(self):
@@ -20,6 +21,7 @@ class JBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
             3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
         }
+        self.move(0, 3)
 
 class IBlock(Block):
     def __init__(self):
@@ -28,8 +30,9 @@ class IBlock(Block):
             0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
             1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
             2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
-            3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(3, 1)]
+            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
         }
+        self.move(-1, 3)
 
 class OBlock(Block):
     def __init__(self):
@@ -40,9 +43,10 @@ class OBlock(Block):
             2: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
             3: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)]
         }
+        self.move(0, 4)
 
 class SBlock(Block):
-    def __init__(self, id):
+    def __init__(self):
         super().__init__(id = 5)
         self.cells = {
             0: [Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)],
@@ -50,6 +54,7 @@ class SBlock(Block):
             2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
             3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+        self.move(0, 3)
 
 class TBlock(Block):
     def __init__(self):
@@ -60,6 +65,7 @@ class TBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
             3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
         }
+        self.move(0, 3)
 
 class ZBlock(Block):
     def __init__(self):
@@ -70,3 +76,4 @@ class ZBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
             3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)]
         }
+        self.move(0, 3)
