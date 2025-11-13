09ba2b03e14d49b87be93a10f0eba234aa010f6c
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-11-12 19:03:58 -0500

    game: se crea interfaz básica para mostrar datos de jugador

--- src/tetris_game/block.py
+++ src/tetris_game/block.py
@@ -38,6 +38,6 @@ class Block:
         tiles = self.get_cell_positions()
         
         for tile in tiles:
-            tile_rect = pygame.Rect(tile.col * self.cell_size + 1, tile.row * self.cell_size + 1,
+            tile_rect = pygame.Rect(tile.col * self.cell_size + 11, tile.row * self.cell_size + 11,
                                     self.cell_size - 1, self.cell_size - 1)
             pygame.draw.rect(screen, self.colors[self.id], tile_rect)
