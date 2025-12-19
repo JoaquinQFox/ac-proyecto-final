37b56295e10b999f9200e3b22c180d8840e8d400
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-12-19 00:02:49 -0500

    game: se crea clase de GestureContrller en archivo aparte del main para organizar código

---
+++ src/tetris_game/gesture_controller.py
@@ -0,0 +1,82 @@
+import pygame
+from gestures import read_gesture
+
+class GestureController:
+    def __init__(self, game):
+        self.game = game
+        self.last_gesture_time = 0
+        self.last_pushdown_time = 0
+        self.last_rotation_time = 0
+
+        # Cooldown para realizar ciertas acciones
+        self.GESTURE_COOLDOWN = 600
+        self.PUSHDOWN_COOLDOWN = 1000
+        self.ROTATION_COOLDOWN = 700
+
+        # Estados de las manos actuales
+        self.estado_mano_izq = "Nada"
+        self.estado_mano_der = "Nada"
+
+        # Contadores de veces que se cierra y abre manos
+        self.contador_cerrar_izq = 0
+        self.contador_cerrar_der = 0
+
+        self.ultimo_gesto_izq = "Nada"
+        self.ultimo_gesto_der = "Nada"
+
+    def update_hands_state(self, gesture):
+        izq_estado = gesture["Left"]
+        der_estado = gesture["Right"]
+
+        if izq_estado == "Sin gesto":
+            self.estado_mano_izq = "Nada"
+        elif izq_estado == "palma_izquierda":
+            self.estado_mano_izq = "Abierta"
+        elif izq_estado == "cerrar_izquierda":
+            self.estado_mano_izq = "Cerrada"
+        else:
+            self.festado_mano_izq = "Inclinada"
+
+        if der_estado == "Sin gesto":
+            self.estado_mano_der = "Nada"
+        elif der_estado == "palma_derecha":
+            self.estado_mano_der = "Abierta"
+        elif der_estado == "cerrar_derecha":
+            self.estado_mano_der = "Cerrada"
+        else:
+            self.estado_mano_der = "Inclinada"
+
+    def gesture_input(self, frame):
+        if self.game.game_over:
+            return
+
+        now = pygame.time.get_ticks()
+        if now - self.last_gesture_time < self.GESTURE_COOLDOWN:
+            return
+
+        self.last_gesture_time = now
+        gesture = read_gesture(frame)
+
+        self.update_hands_state(gesture)
+
+        if "Sin gesto" in gesture.values():
+            return
+
+        if (now - self.last_pushdown_time > self.PUSHDOWN_COOLDOWN
+            and gesture["Left"].startswith("cerrar")
+            and gesture["Right"].startswith("cerrar")):
+            self.game.push_down()
+            self.last_pushdown_time = now
+            return
+
+        if gesture["Left"].startswith("cerrar"):
+            self.game.move_left()
+        if gesture["Right"].startswith("cerrar"):
+            self.game.move_right()
+
+        if now - self.last_rotation_time > self.ROTATION_COOLDOWN:
+            if gesture["Left"].startswith("inclinar"):
+                self.game.rotate_left()
+            if gesture["Right"].startswith("inclinar"):
+                self.game.rotate_right()
+            self.last_rotation_time = now
